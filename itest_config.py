15b2029a3b373fafd38f879ca03bd5819108c1f8
Olga Makogonova <oolgamakogonova@gmail.com>
2026-06-13 18:59:22 +0300

    Delete itest_config.py

--- itest_config.py
+++
@@ -1,9 +0,0 @@
-"""Адреса стенда для integration/e2e тестов (из env, с дефолтами под docker compose)."""
-import os
-
-API_URL = os.environ.get("API_URL", "http://localhost:8080")
-CASSANDRA_HOST = os.environ.get("CASSANDRA_HOST", "localhost")
-CASSANDRA_PORT = int(os.environ.get("CASSANDRA_PORT", "9042"))
-KEYSPACE = os.environ.get("CASSANDRA_KEYSPACE", "warehouse")
-KAFKA_BOOTSTRAP = os.environ.get("KAFKA_BOOTSTRAP", "localhost:29092")
-DLQ_TOPIC = os.environ.get("DLQ_TOPIC", "warehouse-events-dlq")
